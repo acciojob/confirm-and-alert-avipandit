7f25e2eeae062a98600549c9e78083814186a1f4
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-03-24 19:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+// Ask for confirmation
+let confirmation = confirm("Do you want to proceed?");
+
+// Check if the user clicked Ok
+if (confirmation == true) {
+  // If the user clicked OK, show an alert saying "You clicked OK. Proceeding..."
+  alert("You clicked OK. Proceeding...");
+} else {
+  // If the user clicked Cancel, show an alert saying "You clicked Cancel. Exiting..."
+  alert("You clicked Cancel. Exiting...");
+}
+
